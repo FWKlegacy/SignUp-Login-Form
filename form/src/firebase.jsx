2374c567b1d5e42a6123da29bd60f037a9fa1ff6
Brevian <Wanjalawafulabrevian@gmail.com>
2024-10-26 20:54:30 +0300

    firebase

---
+++ form/src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBhIHHjE_q-2JsT4Z97gnPxa6EZs-H23aE",
+  authDomain: "authentication-9f57f.firebaseapp.com",
+  projectId: "authentication-9f57f",
+  storageBucket: "authentication-9f57f.appspot.com",
+  messagingSenderId: "735953144703",
+  appId: "1:735953144703:web:5cd880378bf7683c1fa84a",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
